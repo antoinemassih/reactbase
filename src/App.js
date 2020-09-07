384d40eb9b3c615ee146c70c6c5a55a1f7304c47
antoine <antoine.amassih@gmail.com>
2020-09-07 15:40:47 +0400

    Blueprint+Basic Flex Implemented

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ import {TableReorderable} from "./Components/Table";
 import "@blueprintjs/core/lib/scss/variables.scss"
 function App() {
   return (
+<div className="flex-box">
+      <div className="flex-container">
+          <div><header className="header">  <NavbarExample/></header></div>
+          <div>
+              <div className="flex-container2">
+                  <div className="side"><button/></div>
+                  <div className="main"><TableReorderable/></div>
+                  <div className="side">5</div>
+              </div>
+          </div>
+          <div>footer</div>
 
-      <div className="App">
-          <NavbarExample/>
-<div>
-
-    <TableReorderable/>
+      </div>
 </div>
 
-
-
-    </div>
   );
 }
 
